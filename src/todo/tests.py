9cd17b41d5df53d4124a1de4bc1ae27dee5bd55c
Tudor Hoinaru <tudor.hoinaru@gmail.com>
2019-05-17 12:17:32 +0300

    Latest changes

---
+++ src/todo/tests.py
@@ -0,0 +1,32 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from todo.models import TodoItem
+
+
+# Create your tests here.
+def create_item(client):
+    url = reverse('todoitem-list')
+    data = {'title': 'Walk the dog'}
+    return client.post(url, data, format='json')
+
+
+class TestCreateTodoItem(APITestCase):
+    """
+    Ensure we can create a new todo item
+    """
+
+    def setUp(self):
+        self.response = create_item(self.client)
+
+    def test_received_201_created_status_code(self):
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_received_location_header_hyperlink(self):
+        self.assertRegexpMatches(self.response['Location'], '^http://.+/todos/[\d]+$')
+
+    def test_item_was_created(self):
+        self.assertEqual(TodoItem.objects.count(), 1)
+
+    def test_item_has_correct_title(self):
+        self.assertEqual(TodoItem.objects.get().title, 'Walk the dog')
